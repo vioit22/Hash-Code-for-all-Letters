5a3f7db52c4b83b0907234d54ad0d421c55bcd07
Vio IT <150695785+vioit22@users.noreply.github.com>
2024-09-17 15:47:30 +0300

    Add files via upload

---
+++ Hash Code for all Letters/Program.cs
@@ -0,0 +1,123 @@
+﻿Console.WriteLine("Hash Code for all Letters, developed by Vio IT");
+// For letter "a"
+string a = "a";
+Console.WriteLine("For letter a, Hash Code is:");
+Console.WriteLine(a.GetHashCode());
+// For letter "â"
+string â = "â";
+Console.WriteLine("For letter â, Hash Code is:");
+Console.WriteLine(â.GetHashCode());
+// For letter ă
+string ă = "ă";
+Console.WriteLine("For letter ă, Hash Code is:");
+Console.WriteLine(ă.GetHashCode());
+// For letter "b"
+string b = "b";
+Console.WriteLine("For letter b, Hash Code is:");
+Console.WriteLine(b.GetHashCode());
+// For letter c
+string c = "c";
+Console.WriteLine("For letter c, Hash Code is:");
+Console.WriteLine(c.GetHashCode());
+// For letter d
+string d = "d";
+Console.WriteLine("For letter d, Hash Code is:");
+Console.WriteLine(d.GetHashCode());
+// For letter e
+string e = "e";
+Console.WriteLine("For letter e, Hash Code is:");
+Console.WriteLine(e.GetHashCode());
+// For letter f
+string f = "f";
+Console.WriteLine("For letter f, Hash Code is:");
+Console.WriteLine(f.GetHashCode());
+// For letter g
+string g = "g";
+Console.WriteLine("For letter g, Hash Code is:");
+Console.WriteLine(g.GetHashCode());
+// For letter h
+string h = "h";
+Console.WriteLine("For letter h, Hash Code is:");
+Console.WriteLine(h.GetHashCode());
+// For letter i
+string i = "i";
+Console.WriteLine("For letter i, Hash Code is:");
+Console.WriteLine(i.GetHashCode());
+// For letter î
+string î = "î";
+Console.WriteLine("For letter î, Hash Code is:");
+Console.WriteLine(î.GetHashCode());
+// For letter j
+string j = "j";
+Console.WriteLine("For letter j, Hash Code is:");
+Console.WriteLine(j.GetHashCode());
+// For letter k
+string k = "k";
+Console.WriteLine("For letter k, Hash Code is:");
+Console.WriteLine(k.GetHashCode());
+// For letter l
+string l = "l";
+Console.WriteLine("For letter l, Hash Code is:");
+Console.WriteLine(l.GetHashCode());
+// For letter m
+string m = "m";
+Console.WriteLine("For letter m, Hash Code is:");
+Console.WriteLine(m.GetHashCode());
+// For letter n
+string n = "n";
+Console.WriteLine("For letter n, Hash Code is:");
+Console.WriteLine(n.GetHashCode());
+// For letter o
+string o = "o";
+Console.WriteLine("For letter o, Hash Code is:");
+Console.WriteLine(o.GetHashCode());
+// For letter p
+string p = "p";
+Console.WriteLine("For letter p, Hash Code is:");
+Console.WriteLine(p.GetHashCode());
+// For letter q
+string q = "q";
+Console.WriteLine("For letter q, Hash Code is:");
+Console.WriteLine(q.GetHashCode());
+// For letter r
+string r = "r";
+Console.WriteLine("For letter r, Hash Code is:");
+Console.WriteLine(r.GetHashCode());
+// For letter s
+string s = "s";
+Console.WriteLine("For letter s, Hash Code is:");
+Console.WriteLine(s.GetHashCode());
+// For letter t
+string t = "t";
+Console.WriteLine("For letter t, Hash Code is:");
+Console.WriteLine(t.GetHashCode());
+// For letter ț
+string ț ="ț";
+Console.WriteLine("For letter ț, Hash Code is:");
+Console.WriteLine(ț.GetHashCode());
+// For letter u
+string u = "u";
+Console.WriteLine("For letter u, Hash Code is:");
+Console.WriteLine(u.GetHashCode());
+// For letter v
+string v = "v";
+Console.WriteLine("For letter v, Hash Code is:");
+Console.WriteLine(v.GetHashCode());
+// For letter w
+string w = "w";
+Console.WriteLine("For letter w, Hash Code is:");
+Console.WriteLine(w.GetHashCode());
+// For letter x
+string x = "x";
+Console.WriteLine("For letter x, Hash Code is:");
+Console.WriteLine(x.GetHashCode());
+// For letter y
+string y = "y";
+Console.WriteLine("For letter y, Hash Code is:");
+Console.WriteLine(y.GetHashCode());
+// For letter z
+string z = "z";
+Console.WriteLine("For letter z, Hash Code is:");
+Console.WriteLine(z.GetHashCode());
+Console.WriteLine("Press Enter to exit... Developed by Vio IT");
+Console.ReadLine();
